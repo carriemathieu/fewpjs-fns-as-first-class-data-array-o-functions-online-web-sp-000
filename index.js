312629caefbe9664efe0bb973b5b1eb7c8d6b02a
Carrie Mathieu <carrieburks@gmail.com>
2021-03-11 22:48:19 -0500

    commit tree cb6e3ca2498034ae4efc0024a9ff1eca4425d012

---
+++ index.js
@@ -0,0 +1,14 @@
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+   
+    if (right > 0) {
+      dodger.style.right = `${right + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
